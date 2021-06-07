a405c460253689a02698172aa5b97106fdd46ac1
mirrorcall96 <mirrorcall96@gmail.com>
2021-06-07 22:52:48 +0300

    added dwarfs

--- src/components/main.js
+++ src/components/main.js
@@ -4,14 +4,19 @@ import heroLogo2 from '../images/hero2.png';
 import heroLogo3 from '../images/hero3.png';
 
 import villianLogo from '../images/villian.png';
+import villianLogo2 from '../images/villian2.png';
+import villianLogo3 from '../images/villian3.png';
+//import villianLogo4 from '../images/villian4.png';
+
 import attack from '../images/attack.png';
 import {StyledCard} from '../styles'
 
 
 const Main =(props)=> {
     let cards=[];
+    let vlogos = [villianLogo,villianLogo2,villianLogo3]
     for (let i = 0; i < Math.min(props.villains.length,9); i+=3) {
-        cards.push(<Col xs={2}>{props.villains.filter((a,j) => j>=i && j<i+3).map(a=><StyledCard className="villian" key={a} onClick={()=>props.attackOnVillian(a)} ><StyledCard.Img src={villianLogo} /><StyledCard.Title><center>{a}</center></StyledCard.Title></StyledCard>)}</Col>)
+        cards.push(<Col xs={2}>{props.villains.filter((a,j) => j>=i && j<i+3).map(a=><StyledCard className="villian" key={a} onClick={()=>props.attackOnVillian(a)} ><StyledCard.Img src={vlogos[a % vlogos.length]} /><StyledCard.Title><center>{a}</center></StyledCard.Title></StyledCard>)}</Col>)
     }
     let LevelLogo=heroLogo;
     if(props.level >= 5) LevelLogo=heroLogo2;
